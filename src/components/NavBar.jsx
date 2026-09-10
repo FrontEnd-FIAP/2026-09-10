8e032eeae645a5ab2fdadda2703bd6d2b658a23a
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-09-10 09:33:05 -0300

    feat: Criando as rotas do projeto

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    return(
+        <>
+            <Link to="/">Home</Link>
+            <Link to="/sobre">Sobre</Link>
+            <Link to="/produto">Produto</Link>
+            <Link to="/contato">Contato</Link>
+        </>
+    )
+}
+
+export default Navbar
