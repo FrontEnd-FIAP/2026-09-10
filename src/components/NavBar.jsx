223542f7d34b832c3f0f3c574c2b0cd9d54328d1
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-09-10 09:53:41 -0300

    style: Estilizando o projeto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,30 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import {Home, Info} from 'lucide-react'
 
-const Navbar = () => {
-    return(
-        <>
-            <Link to="/">Home</Link>
-            <Link to="/sobre">Sobre</Link>
-            <Link to="/produto">Produto</Link>
-            <Link to="/contato">Contato</Link>
-        </>
+const NavBar = () => {
+    return (
+        <nav className=" bg-blue-500 shadow-md">
+            <div className="max-w-6xl mx-auto px-4">
+                <div className="flex justify-between h-16">
+                    <div className="flex items-center">
+                        <span className="text-xl font-bold text-white">Projeto</span>
+                    </div>
+                </div>
+
+                <div className=" flex space-x-4 items-center">
+                    <Link to="/" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Home/>Home</Link>
+                    <Link to="/sobre" className="flex items-center px-3 py-2 font-medium hover:text-amber-300"><Info/>Sobre</Link>
+                    <Link to="/produto" className="flex items-center px-3 py-2 font-medium hover:text-amber-300">Produto</Link>
+                    <Link to="/contato" className="flex items-center px-3 py-2 font-medium hover:text-amber-300">Contato</Link>
+
+                </div>
+
+
+
+            </div>
+
+        </nav>
     )
 }
 
-export default Navbar
+export default NavBar
