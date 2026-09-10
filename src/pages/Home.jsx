45b6364f3b509f7852cbc9c6a6ab875f69bf24bd
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-09-10 08:54:33 -0300

    feat: Componentes do Projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <>
+      <h1>Bem-Vindo <span>Home</span></h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Produto.jsx
@@ -0,0 +1,10 @@
+
+const Produto = () => {
+  return (
+    <>
+      <h1>Nosso Produto</h1>
+    </>
+  )
+}
+
+export default Produto
